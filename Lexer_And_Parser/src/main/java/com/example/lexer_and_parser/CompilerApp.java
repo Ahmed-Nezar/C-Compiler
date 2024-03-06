120bc4a8279084c287afb654743e46f58165ab53
Omar Alaa Elnahass <21p0197@eng.asu.edu.eg>
2024-03-07 00:13:39 +0200

    add: Icon to the application

--- Lexer_And_Parser/src/main/java/com/example/lexer_and_parser/CompilerApp.java
+++ Lexer_And_Parser/src/main/java/com/example/lexer_and_parser/CompilerApp.java
@@ -3,6 +3,7 @@ package com.example.lexer_and_parser;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,9 @@ public class CompilerApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CompilerApp.class.getResource("CompilerApplication.fxml"));
+        Image icon = new Image(getClass().getResourceAsStream("lexer_icon.png"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 600);
+        stage.getIcons().add(icon);
         stage.setMinWidth(1200);
         stage.setMinHeight(600);
         stage.setTitle("C Lexer & Parser");
